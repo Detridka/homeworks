5bccd7a2d4a36d1a429ff10d1cb915694ae5bf53
Detridka <dr.takers2809@gmail.com>
2024-07-28 16:25:02 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+spend_time_for_homework = 1.5
+course = 'Pyton'
+amount_completed_HW = 12
+time_for_exmple = (spend_time_for_homework / amount_completed_HW)
+print(course, ',' ' всего задач', amount_completed_HW, ',', 'среднее время выполнения ', time_for_exmple, 'часа')
