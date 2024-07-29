5636a476f0edd3e3c6e261115690acf7a52de233
Detridka <dr.takers2809@gmail.com>
2024-07-29 15:08:11 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,18 @@
+name = 'Rashit'
+print(name, type(name))
+age = 29
+print(age, type(age))
+age = 29+3
+print(age, type(age))
+is_student = True
+print(is_student, type (is_student))
+
+name = 'Rashit'
+print('name:', name)
+age = 29
+print('age:', age)
+age = 29+3
+print('new age:',age)
+is_student = True
+print('Is student:', is_student)
+
