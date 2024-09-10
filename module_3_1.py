c538e4058cf6e3c4013544bebb0725f0354e98e9
Detridka <dr.takers2809@gmail.com>
2024-09-10 12:05:48 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,19 @@
+call = 0
+def count_call():
+    global call
+    call += 4
+def string_info(string):
+    return len(string), string.upper(), string.lower()
+def is_contains(string, list_to_search):
+    for item in list_to_search:
+        if item.casefold() == string.casefold():
+            return True
+    return False
+count_call()
+
+print(string_info('dva'))
+print(string_info('krevetka'))
+print(is_contains('Бар', ['бАР', 'Банан', 'Вася']))
+print(is_contains('круг', ['квадрат', 'треугольник']))
+print(call)
+
